9e8fbbf7c4631a5bbacecf16ffca565702896c35
brightigiemokha <brightigiemokha@yahoo.com>
2023-10-21 10:18:03 +0000

    add question 2 to 4

--- run.py
+++ run.py
@@ -11,14 +11,40 @@ display_question(questions[0])
 
 # list of questions
 questions = [
-# question 1
+    # question 1
     {
-        'question':'What does CPU stands for?',
-        'a':'Central Processing Unit',
-        'b':'Open Systems Interconnect',
-        'c':'Organised Stairway Interweb',
-        'd':'Open Safe Internet',
-        'correct':'a',
+        'question': 'What does CPU stands for?',
+        'a': 'Central Processing Unit',
+        'b': 'Open Systems Interconnect',
+        'c': 'Organised Stairway Interweb',
+        'd': 'Open Safe Internet',
+        'correct': 'a',
     },
-
+    # question 2
+    {
+        'question': 'What is a Computer?',
+        'a': 'Computer is a software',
+        'b': 'Open Systems Interconnect',
+        'c': 'An Electronic device used in processing data',
+        'd': 'An Internet device',
+        'correct': 'c',
+    },
+    # question 3
+    {
+        'question': 'Who released their first antivirus product called VirusScan in 1987?',
+        'a': 'John wesley',
+        'b': 'John McAfee',
+        'c': 'Norton',
+        'd': 'Bill Gate',
+        'correct': 'b',
+    },
+    # question 3
+    {
+        'question': 'What word means to switch a computer off and on again?',
+        'a': 'Hibenate',
+        'b': 'Sleep',
+        'c': 'Reboot',
+        'd': 'shut down',
+        'correct': 'b',
+    },
 ]
