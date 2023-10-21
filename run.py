4fdcdd8658c68d8b07a738260a0d44741eaed760
brightigiemokha <brightigiemokha@yahoo.com>
2023-10-21 10:09:50 +0000

    add question function and question 1

--- run.py
+++ run.py
@@ -8,3 +8,17 @@ if playing.lower() != "yes":
 print("Great Choice! Lets Play :)")
 
 display_question(questions[0])
+
+# list of questions
+questions = [
+# question 1
+    {
+        'question':'What does CPU stands for?',
+        'a':'Central Processing Unit',
+        'b':'Open Systems Interconnect',
+        'c':'Organised Stairway Interweb',
+        'd':'Open Safe Internet',
+        'correct':'a',
+    },
+
+]
